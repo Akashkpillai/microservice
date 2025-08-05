659b468f3e99a4855006db42147ada0b732fed1e
Akashkpillai <109843806+Akashkpillai@users.noreply.github.com>
2025-08-06 02:31:00 +0530

    fix docker

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(ReservationModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.port ?? 3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
